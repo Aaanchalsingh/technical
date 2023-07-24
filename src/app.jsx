41a0743e2252291e6592878fc06d32e94780e83f
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-07-24 21:43:17 +0530

    latest

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Routes , Route } from "react-router-dom";
-import Services from './Services';
-import Contact from './Contact';
-import Home from './Home';
-import Footer from './footer';
-import About from './About';
+import React from "react";
+import Services from "./Services";
+import Contact from "./Contact";
+import Home from "./Home";
+import Footer from "./footer";
+import About from "./About";
 
 function app() {
   return (
     <>
-    <Routes>
-      <Route exact path='/' element={<Home title="Technicals"/>}/>
-      <Route exact path='/about' element={<About/>}/>
-      <Route exact path='/services' element={<Services/>}/>
-      <Route exact path='/contact' element={<Contact/>}/>
-    </Routes>
+      <Home title="Technicals" />
+      <About />
+      <Services />
+      <Contact />
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default app
+export default app;
